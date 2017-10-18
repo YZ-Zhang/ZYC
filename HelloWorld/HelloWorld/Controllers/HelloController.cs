e72042f60b99b599d71cc5ae4480f0b8ef15e8a3
YZ-Zhang <32892083+YZ-Zhang@users.noreply.github.com>
2017-10-18 15:40:20 +0800

    1st

---
+++ HelloWorld/HelloWorld/Controllers/HelloController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HelloWorld.Controllers
+{
+    public class HelloController : Controller
+    {
+        // GET:/ Hello/
+        public ActionResult Index()
+        {
+            return View ();
+        }
+        public ActionResult Welcomr(string name, int times = 1)
+        {
+            ViewBag.msg = "你好" + name + "!";
+            ViewBag.times = times;
+            return View();
+        }
+    }
+}
